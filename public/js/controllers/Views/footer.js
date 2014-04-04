b84b8f3355ceebbca735c35c6b0194d3d6210b8b
everythingspirals <dushan.perera.26@gmail.com>
2014-04-04 01:22:38 -0700

    gitfix

--- public/js/controllers/Views/footer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('mean.system')
-
-.controller('FooterController', ['$scope', 'Global', 'socket', function ($scope, Global, socket) {
-    $scope.global = Global;
-
-    $scope.chatToggle = false;
-}]);
--- public/js/controllers/Views/sidenav.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('mean.system').controller('SidenavController', ['$scope', 'Global', function ($scope, Global) {
-    $scope.global = Global;
-
-}]);
